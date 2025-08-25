31c8ee43c8b9eb19d1f4dee8bb0cda1449747575
synergic <64419373+synergic777@users.noreply.github.com>
2025-08-25 22:45:00 +0200

    Update rclone.ps1

--- rclone.ps1
+++ rclone.ps1
@@ -1,4 +1,4 @@
 while ($true) {
-    rclone sync "$env:USERPROFILE\Videos" gdrive:PC-Videos --progress
+    rclone sync "$env:USERPROFILE\Videos" gdrive:PC-Videos --progress --bwlimit 1.25M
     Start-Sleep -Seconds 300  # Wait 5 minutes
 }
