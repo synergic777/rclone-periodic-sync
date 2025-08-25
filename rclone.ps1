56292179521fb69ec994ea05c8a94d71ed567b63
synergic <64419373+synergic777@users.noreply.github.com>
2025-08-25 23:03:48 +0200

    Update rclone.ps1

--- rclone.ps1
+++ rclone.ps1
@@ -1,4 +1,15 @@
 while ($true) {
-    rclone sync "$env:USERPROFILE\Videos" gdrive:PC-Videos --progress --bwlimit 1.25M
-    Start-Sleep -Seconds 300  # Wait 5 minutes
+    $files = Get-ChildItem "$env:USERPROFILE\Videos" -File
+
+    foreach ($file in $files) {
+        # Copy single file
+        rclone copy "$($file.FullName)" gdrive:PC-Videos --progress --bwlimit 1.25M
+
+        # Delete local file only if copy succeeded
+        if ($LASTEXITCODE -eq 0) {
+            Remove-Item "$($file.FullName)" -Force
+        }
+    }
+
+    Start-Sleep -Seconds 300
 }
