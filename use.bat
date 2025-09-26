21b891017434de759d79d8257f1529a1e7bbc6be
synergic <64419373+synergic777@users.noreply.github.com>
2025-09-26 11:27:21 +0200

    Update use.bat

--- use.bat
+++ use.bat
@@ -5,6 +5,8 @@ SET DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\rclone.lnk
 IF EXIST "%SRC%" (
     COPY /Y "%SRC%" "%DEST%"
     ECHO rclone.lnk has been copied to Startup.
+    REM Open the Startup folder in File Explorer
+    START "" "C:\Users\noisy\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 ) ELSE (
     ECHO rclone.lnk does not exist in the current directory.
 )
